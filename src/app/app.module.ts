41e071846b01154d0b0eb9c3ab271de88f3af8d2
Marcos Volpato <volpatomv@gmail.com>
2020-05-24 16:28:17 -0300

    [task-35] - Adicionado suporte para material icons outlineds - Adicionado paths no arquivo tsconfig para facilitar imports - Adicionado as páginas do menu (sem conteúdo ainda) - Adicionado link para a home no title do header - Adicionado estilo para o link do menu do header para as rotas ativas - Criado componente offer-card e inserido na home (mockado)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { AdvertiseComponent } from './advertise/advertise.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    AdvertiseComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
