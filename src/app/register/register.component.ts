70a876b62131d56c20987316711c0aa03b8f6b42
Ricardo Atakiama <atakiama@usp.br>
2020-06-30 15:00:36 -0300

    pag. de registro c. upload / novos campos p. user

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -24,7 +24,10 @@ export class RegisterComponent implements OnInit {
       nome: new FormControl(user.nome),
       email: new FormControl(user.email),
       password: new FormControl(user.password),
-      repassword: new FormControl(user.password)
+      repassword: new FormControl(user.password),
+      userImg: new FormControl(user.userImg),
+      cell: new FormControl(user.cell),
+      aboutMe: new FormControl(user.aboutMe)
     });
   }
 
@@ -32,15 +35,21 @@ export class RegisterComponent implements OnInit {
     if (this.formUserRegister.controls.password.value === this.formUserRegister.controls.repassword.value) {
       this.register( this.formUserRegister.controls.nome.value,
                   this.formUserRegister.controls.email.value,
-                  this.formUserRegister.controls.password.value).subscribe();
+                  this.formUserRegister.controls.password.value,
+                  this.formUserRegister.controls.userImg.value,
+                  this.formUserRegister.controls.cell.value,
+                  this.formUserRegister.controls.aboutMe.value).subscribe();
     }
   }
 
-  register(nome: string, email: string, password: string): Observable<any> {
+  register(nome: string, email: string, password: string, userImg: string, cell: string, aboutMe: string): Observable<any> {
     const formData = new FormData();
     formData.append('nome', nome);
     formData.append('email', email);
     formData.append('password', password);
+    formData.append('userImg', userImg);
+    formData.append('cell', cell);
+    formData.append('aboutMe', aboutMe);
 
     console.warn(this.formUserRegister.value); // pode apertar enter para submeter
     return this.http.post<any>('http://52.67.36.1/add_user', formData).pipe(
@@ -48,5 +57,9 @@ export class RegisterComponent implements OnInit {
         console.log('response -> ', response);
       })
     );
+  }
+
+  fotoEnviada(event: any) {
+    this.formUserRegister.controls.userImg.setValue(event.url);
   }
 }
--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -2,4 +2,7 @@ export class User {
   nome: any = '';
   email: any = '';
   password: any = '';
+  userImg: any = '';
+  cell: any = '';
+  aboutMe: any = '';
 }
