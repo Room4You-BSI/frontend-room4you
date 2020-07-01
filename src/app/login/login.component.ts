1a22f93e6947dc3725e0c78a07849e597e9fcc13
Ricardo Atakiama <atakiama@usp.br>
2020-07-01 16:23:05 -0300

    Ajustes de estilos + msgs erro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,13 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { tap } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 
 import { AuthService } from '../shared/services/auth.service';
 import { User } from '../users/user';
+import { throwError } from 'rxjs';
 
 
 @Component({
@@ -15,6 +17,7 @@ import { User } from '../users/user';
 })
 export class LoginComponent implements OnInit {
   formUserLogin: FormGroup;
+  erro: HttpErrorResponse;
 
   constructor(
     private authService: AuthService,
@@ -33,9 +36,14 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmitLogin() {
+    this.erro = null;
     console.warn(this.formUserLogin.value); // pode apertar enter para submeter
     this.authService.login(this.formUserLogin.controls.email.value, this.formUserLogin.controls.password.value).pipe(
       tap(() => this.router.navigate([''])),
+      catchError((err) => {
+        this.erro = err;
+        return throwError(err);
+      })
     ).subscribe();
   }
 }
